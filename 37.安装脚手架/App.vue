8f71b7b30ab604a3d46107977751f785d9b477d7
chen <2463238851@qq.com>
2021-02-09 11:32:30 +0800

    37.安装脚手架

---
+++ 37.安装脚手架/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>xxxx</div>
+    {{ name }}
+    <button @click="handleClick">click me</button>
+    <p>我是父组件的的p元素</p>
+
+    <my-son></my-son>
+  </div>
+</template>
+<script>
+import son from "./son.vue";
+export default {
+  data() {
+    return {
+      name: "chen",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(123);
+    },
+  },
+  components: {
+    mySon: son,
+  },
+};
+</script>
+<style>
+p {
+    background: red;
+}
+</style>
---
+++ 37.安装脚手架/son.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>我是子组件的p元素</p>
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "son",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+    background: rgb(136, 255, 0);
+}
+</style>
